fb70111e4d17e5e1ae3dbfa15999e3477cdedbf4
Jacques Berger <jacques.berger@code3.ca>
2023-07-13 22:53:00 -0400

    Changer l'année de copyright

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-# Copyright 2022 <Votre nom et code permanent>
+# Copyright 2023 <Votre nom et code permanent>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
